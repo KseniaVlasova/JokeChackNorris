bb7b382aa774478d41d91f3ff8e27ea7875e9c59
KseniaVlasova <KseniaVlasova97@yandex.ru>
2022-05-16 12:45:14 +0300

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChackNorris';
+  joke:string='';
+  
+  constructor(private httpService: HttpService){}
+
+  getJoke(){
+    this.httpService.getData().subscribe(v => this.writeToJoke(v));
+  }
+
+  writeToJoke(obj:any){
+    this.joke = obj['value'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule }   from '@angular/common/http';
+import { HttpService } from './http.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
